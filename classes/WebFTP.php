ccc275a841478732f6f63718d721d8e66e54d2e6
Nemanja Maric <nemanjammaric@gmail.com>
2016-03-08 14:19:35 +0100

    Version 2

--- classes/WebFTP.php
+++ classes/WebFTP.php
@@ -2,8 +2,9 @@
 
 class ftpFile
 {
-	protected $_conn; //Connect to ip and port ftp
+	public $_conn; //Connect to ip and port ftp
 	protected $_user; // Connect user to ftp
+	public $__ftp;
 	public $put;
 	
 	//************ Connect to ftp
@@ -11,12 +12,13 @@ class ftpFile
 	{
 		$this->_conn = ftp_connect($ftp_server, $ftp_port);
 		$this->_user = ftp_login($this->_conn, $username, $pass);
+		ftp_pasv($this->_conn, true);
+		$this->__ftp = "ftp://$username:$pass@$ftp_server:$ftp_port";
 	}
 	
 	public function Patch($patch)
 	{
-		$this->put = $patch;
-		return ftp_chdir($this->_conn, $patch);
+		return $this->put = chdir($patch);
 	}
 	
 	public function Upload($file,$file_Name)
@@ -73,19 +75,42 @@ class ftpFile
 	protected $_read;
 	public $readText;
 	
-	public function Read($RFile){
-		$this->_read = fopen($RFile, "r");
-		$this->readText = fread($this->_read, filesize($RFile));
-		return $this->readText;
-	}
-	
-	protected $_write;
-	
-	public function Write($Wfile, $Wtext){
-		$this->_write = fopen($Wfile, "w+");
-		fwrite($this->_write, stripslashes($Wtext));
-		return true;
+	public function Read($RFile)
+	{
+		$this->_read = fopen($this->__ftp , "r");
+		$this->readText = fread($this->_read, filesize($this->__ftp));
+		echo $this->readText;
 	}
 	
 	
+	public static function Write($Wfile, $Wtext)
+	{
+		$context = stream_context_create(array('ftp'=>array('overwrite' => true)));
+		$open = fopen($Wfile , "w", false, $context);
+		return fwrite($open, $Wtext);
+	}
+	
+	public $test;
+	
+	public function Give_file($file)
+	{
+		$array = ftp_rawlist($this->_conn, $file);
+		$array2 = join("\n", $array);
+		preg_match_all('/^([drwx+-]{10})\s+(\d+)\s+(\w+)\s+(\w+)\s+(\d+)\s+(.{12}) (.*)$/m', $array2, $this->test, PREG_SET_ORDER);
+		return $this->test;
+	}
+	
+	private $__create;
+	
+	public function Createfile($name)
+	{
+		$this->__create = fopen($name,"w+");
+		fwrite($this->__create, "/* Storm Hosting New File */");
+	}
+	
+	public function Promeni($njega)
+	{
+		$this->__ftp .=  $njega;
+		return ($this->__ftp);
+	}
 }
